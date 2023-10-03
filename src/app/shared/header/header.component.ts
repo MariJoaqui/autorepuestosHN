4fb347c65a0927ab6a76d8f6408b4a11477da4e8
Maria Joaquina <morenoaulaciomariajoaquina@gmail.com>
2023-10-03 12:17:13 -0400

    Creacion del navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+// Interfaces
+import { Items } from '../interfaces/interfaces';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  navItems: Items[] = [
+    { title: 'Sobre nosotros', link: '' },
+    { title: 'Servicios', link: '' },
+    { title: 'Ubicanos', link: '' },
+    { title: 'Contactanos', link: '' }
+  ];
 
 }
